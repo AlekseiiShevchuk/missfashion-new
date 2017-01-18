b1c564f77c6a16aaa519eca0f90941708f364543
alekseiishevchuk <shevchuka@gmail.com>
2017-01-18 15:39:05 +0200

    add search

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Category;
 use App\Product;
 use Illuminate\Http\Request;
 
@@ -12,17 +13,32 @@ class FrontController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        $products = Product::paginate(16);
-        return view('front.index',['products' => $products]);
+        $categories = Category::all()->take(7)->pluck('name', 'id');
+        $where = [];
+
+        if ($request->get('cat')) {
+            $category = Category::findOrFail((int)$request->get('cat'));
+            $where[] =['category_id','=',(int)$request->get('cat')];
+        }
+
+        if ($request->get('search')) {
+            $where[] = ['name', 'like', $request->get('search').'%'];
+        }
+
+        $products = Product::where($where)->paginate(16);
+        return view('front.index', [
+            'products' => $products,
+            'categories' => $categories,
+        ]);
     }
 
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function show($id)
--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -23,13 +23,9 @@
 <div class="top-bar">
     <div class="top-bar-left">
         <ul class="menu">
-            <li class="menu-text">Missfashion</li>
-            <li><a href="#">One</a></li>
-            <li><a href="#">Two</a></li>
-            <li><a href="#">Three</a></li>
-            <li><a href="#">Four</a></li>
-            <li><a href="#">Five</a></li>
-            <li><a href="#">Six</a></li>
+@foreach($categories as $key => $value)
+            <li><a href="{{route('main')}}?cat={{$key}}">{{$value}}</a></li>
+@endforeach
         </ul>
     </div>
 </div>
