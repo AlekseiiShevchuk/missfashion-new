af1e9960dff2c8f2b6fd003ead9e722a2ebf29fb
alekseiishevchuk <shevchuka@gmail.com>
2017-02-15 17:05:39 +0200

    delete useless files

--- public/revslider/application/views/media/gallery_dialog.php
+++
@@ -1,6 +0,0 @@
-<div class="gallery">
-	<div id="gallery_content"><?php echo $output; ?></div>
-	<div class="alignright">
-		<a href="#" class="button-primary revblue insert-button" disabled="disabled">Insert</a>
-	</div>
-</div>
