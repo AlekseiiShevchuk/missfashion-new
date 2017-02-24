98c3bbdb7310f1ab414b11122ba6ada56a6b6bf2
alekseiishevchuk <shevchuka@gmail.com>
2017-02-24 14:05:47 +0200

    translate

--- resources/views/front/cats.blade.php
+++ resources/views/front/cats.blade.php
@@ -7,22 +7,22 @@
                         <li>
                             <span class="current-li">
                                 <span class="current-li-content">
-                                    <a aria-haspopup="true">Sort by <strong>...</strong></a>
+                                    <a aria-haspopup="true">Sorter efter <strong>...</strong></a>
                                 </span>
                                 <i class="fa fa-angle-down" aria-hidden="true"></i>
                             </span>
                             <ul>
 @if(Route::currentRouteName() == 'main')
-    <li><a href="{{route('main')}}/?sort=priceHF&search={{request()->get('search')}}">Sort by <strong>Price: Highest first</strong></a></li>
-    <li><a href="{{route('main')}}/?sort=priceLF&search={{request()->get('search')}}">Sort by <strong>Price: Lowest first</strong></a></li>
-    <li><a href="{{route('main')}}/?sort=nameAZ&search={{request()->get('search')}}">Sort by <strong>Name: A-Z</strong></a></li>
-    <li><a href="{{route('main')}}/?sort=nameZA&search={{request()->get('search')}}">Sort by <strong>Name: Z-A</strong></a></li>
+    <li><a href="{{route('main')}}/?sort=priceHF&search={{request()->get('search')}}">Sorter efter <strong>Pris: Højeste først</strong></a></li>
+    <li><a href="{{route('main')}}/?sort=priceLF&search={{request()->get('search')}}">Sorter efter <strong>Pris: Laveste først</strong></a></li>
+    <li><a href="{{route('main')}}/?sort=nameAZ&search={{request()->get('search')}}">Sorter efter <strong>Efter bogstaver: A til Å</strong></a></li>
+    <li><a href="{{route('main')}}/?sort=nameZA&search={{request()->get('search')}}">Sorter efter <strong>Efter bogstaver: Å til A</strong></a></li>
 @endif
 @if(Route::currentRouteName() == 'category')
-    <li><a href="{{route('category',['category'=> Route::input('category')])}}/?sort=priceHF&search={{request()->get('search')}}">Sort by <strong>Price: Highest first</strong></a></li>
-    <li><a href="{{route('category',['category'=> Route::input('category')])}}/?sort=priceLF&search={{request()->get('search')}}">Sort by <strong>Price: Lowest first</strong></a></li>
-    <li><a href="{{route('category',['category'=> Route::input('category')])}}/?sort=nameAZ&search={{request()->get('search')}}">Sort by <strong>Name: A-Z</strong></a></li>
-    <li><a href="{{route('category',['category'=> Route::input('category')])}}/?sort=nameZA&search={{request()->get('search')}}">Sort by <strong>Name: Z-A</strong></a></li>
+    <li><a href="{{route('category',['category'=> Route::input('category')])}}/?sort=priceHF&search={{request()->get('search')}}">Sorter efter <strong>Pris: Højeste først</strong></a></li>
+    <li><a href="{{route('category',['category'=> Route::input('category')])}}/?sort=priceLF&search={{request()->get('search')}}">Sorter efter <strong>Pris: Laveste først</strong></a></li>
+    <li><a href="{{route('category',['category'=> Route::input('category')])}}/?sort=nameAZ&search={{request()->get('search')}}">Sorter efter <strong>Efter bogstaver: A til Å</strong></a></li>
+    <li><a href="{{route('category',['category'=> Route::input('category')])}}/?sort=nameZA&search={{request()->get('search')}}">Sorter efter <strong>Efter bogstaver: Å til A</strong></a></li>
 @endif
                             </ul>
                         </li>
